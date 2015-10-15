b14f46112840ab2f08be9f94c39eb317ef9c4605
renzero <a19850728@yahoo.com.tw>
2015-10-15 22:47:11 +0800

    updateBlade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
 
         <title>Laravel</title>
+        
         <link rel=“stylesheet” href=“//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
     </head>
