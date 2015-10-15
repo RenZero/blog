eb62e2a284e97a7189624a8c61e1ec7468858480
renzero <a19850728@yahoo.com.tw>
2015-10-15 22:46:12 +0800

    blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,11 +8,12 @@
         <link rel=“stylesheet” href=“//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
     </head>
     <body>
         <div class="container">
+            appblade Start
             @yield('content')
+            appblade End
         </div>
 
         @yield('footer')
